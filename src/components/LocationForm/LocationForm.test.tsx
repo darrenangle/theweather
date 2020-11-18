09211e8e56c460bc06b7047b0cae32002a47b177
darrenangle <darrendarrenangle@gmail.com>
2020-11-18 00:26:54 -0600

    add location form tests

--- src/components/LocationForm/LocationForm.test.tsx
+++ src/components/LocationForm/LocationForm.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import LocationForm, {LocationFormProps} from './LocationForm';
 
 describe('LocationForm', () => {
+  jest.useFakeTimers('modern');
   function renderLocationForm(props: Partial<LocationFormProps> = {}) {
     const defaults: LocationFormProps = {
       submit: () => null,
@@ -22,7 +23,51 @@ describe('LocationForm', () => {
       ((await findByTestId('location-input')) as HTMLInputElement).value
     ).toBe('');
     expect(
-      (await findByTestId('location-submit-button')).disabled
+      ((await findByTestId('location-submit-button')) as HTMLButtonElement)
+        .disabled
     ).toBeTruthy();
+  });
+
+  test('<LocationForm/> should enable button on input', async () => {
+    // Given
+    const {findByTestId} = renderLocationForm();
+    const input = await findByTestId('location-input');
+    // When
+    fireEvent.change(input, {target: {value: 'some text'}});
+    // Then
+    expect(
+      ((await findByTestId('location-submit-button')) as HTMLButtonElement)
+        .disabled
+    ).toBeFalsy();
+  });
+
+  test('<LocationForm/> should disable the button when loading', async () => {
+    // Given
+    const props = {loading: true};
+    // When
+    const {findByTestId} = renderLocationForm(props);
+    // Then
+    expect(
+      ((await findByTestId('location-submit-button')) as HTMLButtonElement)
+        .disabled
+    ).toBeTruthy();
+  });
+
+  test('<LocationForm/> should call submit with input', async () => {
+    // Given
+    const submit = jest.fn();
+    const props = {submit, loading: false};
+    const {findByTestId} = renderLocationForm(props);
+    const input = await findByTestId('location-input');
+    const button = (await findByTestId(
+      'location-submit-button'
+    )) as HTMLButtonElement;
+
+    // When
+    fireEvent.change(input, {target: {value: 'some text'}});
+    fireEvent.click(button);
+    jest.advanceTimersByTime(500);
+    // Then
+    expect(submit).toHaveBeenCalledWith('some text');
   });
 });
