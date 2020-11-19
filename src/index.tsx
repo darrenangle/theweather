d948c8b428a36186b74755afb09fb9821141d564
darrenangle <darrendarrenangle@gmail.com>
2020-11-19 08:22:45 -0600

    style: smoother transitions between themes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import WeatherApp from './App';
 import './layout-and-styles/reset.css';
 import './layout-and-styles/fonts/fonts.css';
+import './layout-and-styles/theme/theme-transition.css';
 
 import {withReduxStore} from './redux-store/redux-store';
 
