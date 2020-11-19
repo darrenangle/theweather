6569edeea4356be4771eb8d7812af0629f99d45e
darrenangle <darrendarrenangle@gmail.com>
2020-11-19 06:28:21 -0600

    style: high low

--- src/components/DayTempRange/DayTempRange.tsx
+++ src/components/DayTempRange/DayTempRange.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type DayTempRangeProps = {
   morning: number;
@@ -10,14 +11,37 @@ type DayTempRangeProps = {
 const DayTempRange = (props: DayTempRangeProps) => {
   const {morning, day, evening, night} = props;
   return (
-    <>
-      <br />
-      <div>{morning}</div>
-      <div>{day}</div>
-      <div>{evening}</div>
-      <div>{night}</div>
-    </>
+    <Wrapper>
+      <div>
+        <p>{morning}</p>
+        <p>MORN</p>
+      </div>
+      <div>
+        <p>{day}</p>
+        <p>DAY</p>
+      </div>
+      <div>
+        <p>{evening}</p>
+        <p>EVE</p>
+      </div>
+      <div>
+        <p>{night}</p>
+        <p>NIGHT</p>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-content: stetch;
+  margin-top: 40px;
+`;
+const Item = styled.div`
+`
 
 export default DayTempRange;
--- src/components/HighLow/HighLow.tsx
+++ src/components/HighLow/HighLow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type HighLowProps = {
   high: number;
@@ -8,11 +9,60 @@ type HighLowProps = {
 const HighLow = (props: HighLowProps) => {
   const {high, low} = props;
   return (
-    <div>
-      <div>high: {high}</div>
-      <div>low: {low}</div>
-    </div>
+    <Wrapper>
+      <Circle>
+        <Label>HIGH</Label>
+        <Temp>{high}</Temp>
+      </Circle>
+      <Circle>
+        <Label>LOW</Label>
+        <Temp>{low}</Temp>
+      </Circle>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-content: stetch;
+  margin-top: 40px;
+`;
+
+const Circle = styled.div`
+  height: 157px;
+  width: 157px;
+  border-radius: 300px;
+  border: 2px solid ${props => props.theme.panelContent};
+  margin: 0 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+Circle.defaultProps = {
+  theme: {
+    darkBG: '#AFC5CC',
+    panelContent: '#FFFFFF',
+  },
+};
+
+const Label = styled.p`
+  font-family: 'Helvetica Neue', sans-serif;
+  font-weight: 800;
+  font-size: 13px;
+  letter-spacing: 0.05rem;
+  margin-top: 20px;
+`;
+
+const Temp = styled.p`
+  font-family: 'Hatton', serif;
+  font-size: 75px;
+  margin-top: 22px;
+`;
 
 export default HighLow;
