2f806afe23310ecca07e992fc1c6db81e97fe9b1
darrenangle <darrendarrenangle@gmail.com>
2020-11-23 07:39:17 -0600

    chore: clean up imports in redux-store.ts

--- src/redux-store/redux-store.tsx
+++ src/redux-store/redux-store.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import WeatherApp from '../App';
 import AppState, {AppStateKeys} from '../AppState';
-import {Provider, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {createStore} from 'redux';
 
 const {
